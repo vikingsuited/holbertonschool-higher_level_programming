015e9b2655806bbd12fc6ba956a5e0b719008f83
vikingsuited <turkish1396@gmail.com>
2026-04-01 15:52:58 +0000

    Fix W293 blank line whitespace errors

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,13 +3,11 @@ import sys
 
 if __name__ == "__main__":
     count = len(sys.argv) - 1
-    
     if count == 0:
         print("{:d} arguments.".format(count))
     elif count == 1:
         print("{:d} argument:".format(count))
     else:
         print("{:d} arguments:".format(count))
-        
     for i in range(1, count + 1):
         print("{:d}: {:s}".format(i, sys.argv[i]))
