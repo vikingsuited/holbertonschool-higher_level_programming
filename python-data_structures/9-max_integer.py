d339476cc35555a92cd696bcbf1269abf9524921
vikingsuited <turkish1396@gmail.com>
2026-04-10 14:19:51 +0000

    Task 9: Find the max integer in a list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    biggest = my_list[0]
+    for x in my_list:
+        if x > biggest:
+            biggest = x
+    return biggest
