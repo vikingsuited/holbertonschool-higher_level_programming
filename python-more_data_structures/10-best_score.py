4e783621c6c06b63572c0a76f4c674b7837fc2be
vikingsuited <turkish1396@gmail.com>
2026-04-10 14:35:08 +0000

    Task 10: Find the key with the highest value in a dictionary

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    best_key = list(a_dictionary.keys())[0]
+    for key in a_dictionary:
+        if a_dictionary[key] > a_dictionary[best_key]:
+            best_key = key
+    return best_key
