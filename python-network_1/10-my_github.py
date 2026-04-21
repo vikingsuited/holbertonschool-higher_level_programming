09a6883c47e16ed900c6cbdcb763af5fb9142eca
vikingsuited <turkish1396@gmail.com>
2026-04-21 15:38:31 +0000

    Task 9: Access GitHub API user id

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Uses the GitHub API to display the user id using Basic Authentication.
+The script takes GitHub username and Personal Access Token as arguments.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    # HTTP Basic Authentication requests kitabxanası ilə
+    r = requests.get(url, auth=(username, password))
+    
+    try:
+        # Cavabı JSON formatında oxuyuruq
+        user_data = r.json()
+        # Əgər giriş uğurludursa 'id' açarını, deyilsə None çap edirik
+        print(user_data.get('id'))
+    except ValueError:
+        print("None")
