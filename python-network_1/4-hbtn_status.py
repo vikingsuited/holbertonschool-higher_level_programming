cf3ce6982f911692067874649b02acf1147da677
vikingsuited <turkish1396@gmail.com>
2026-04-21 15:22:32 +0000

    Task 4: Fetch status using requests library

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Fetches https://intranet.hbtn.io/status using the requests package.
+"""
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
